804b565440cf4ec5554133ae5b686f870b700d03
Sagar Kumar Sutar <78399767+sagarsutar007@users.noreply.github.com>
2024-03-21 18:15:28 +0530

    Update 2024_02_20_030046_add_re_order_to_materials_table.php

--- database/migrations/2024_02_20_030046_add_re_order_to_materials_table.php
+++ database/migrations/2024_02_20_030046_add_re_order_to_materials_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::table('materials', function (Blueprint $table) {
             //If exists skip    
             if (!Schema::hasColumn('materials', 're_order')) {
-                $table->decimal('re_order', 10, 3)->after('opening_balance')->nullable();
+                $table->decimal('re_order', 10, 3)->after('price')->nullable();
             }
         });
     }
